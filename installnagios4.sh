b66b6581674f5c995319b50d23c6317657784479
Daniel Lara Souza <daniellarasouza@yahoo.com.br>
2016-12-08 14:40:18 -0200

    Create installnagios4.sh

---
+++ installnagios4.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+yum install gd gd-devel gcc glibc glibc-common wget unzip -y
+
+useradd -m nagios
+passwd nagios
+
+groupadd nagcmd
+usermod -a -G nagcmd nagios
+usermod -a -G nagcmd apache
+
+
+wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.2.4.tar.gz
+
+wget https://nagios-plugins.org/download/nagios-plugins-2.1.4.tar.gz
+
+
+tar -zxvf nagios-4.2.4.tar.gz
+
+cd nagios-4.2.4
+
+./configure --prefix=/etc/nagios --with-command-group=nagcmd
+
+make all
+
+make install
+
+make install-init
+
+make install-config
+
+make install-commandmode
+
+make install-webconf
+
+htpasswd -c /etc/nagios/etc/htpasswd.users nagiosadmin
+
+cd ..
+
+tar -zxvf nagios-plugins-2.1.4.tar.gz
+
+cd nagios-plugins-2.1.4
+
+./configure --prefix=/etc/nagios --with-nagios-user=nagios --with-nagios-group=nagios
+
+make
+
+make install
+
+chkconfig nagios on
+
+chkconfig httpd on
+
+service nagios start
+
+service httpd start
+
